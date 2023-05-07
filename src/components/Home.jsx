0ce1955e57ac30cafb45a0b1f30ba6272f208a18
towa0 <126798279+towa0@users.noreply.github.com>
2023-05-07 14:47:20 +0400

    Add files via upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,20 +15,21 @@ const Home = () => {
           I'm a Front End developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I'm a front-end develepor specializing in building (and occasionally
-          designing) exeptional digital expierences. Currently, I'm focused on
-          building responsive front end / full-stack web applications.
+          I'm a front-end developer specializing in building exceptional digital
+          experiences primarily using ReactJS. My focus is on creating
+          responsive front-end and (occasionally) full-stack web applications.
         </p>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-3 flex items-center  hover:border-[#e1ea63]">
-            <Link to="work" smooth={true} duration={500}>
-              View work
-            </Link>
-
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3" />
-            </span>
-          </button>
+          <a className="inline-block">
+            <button className="text-white group border-2 px-6 py-3 my-3 flex items-center hover:border-[#e1ea63]">
+              <Link to="work" smooth={true} duration={500}>
+                View work
+              </Link>
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </a>
         </div>
       </div>
     </div>
