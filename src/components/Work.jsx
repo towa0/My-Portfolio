44f7e52690443ada0fed019e05f4ae3ddd58e889
towa0 <126798279+towa0@users.noreply.github.com>
2023-05-10 20:27:50 +0400

    Add files via upload

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import WorkImg from "../assets/Rentigo.png";
 import myCompany from "../assets/WorkImg2.png";
+import DataPulse from "../assets/data_pulse.png";
 
 const Work = () => {
   return (
@@ -51,7 +52,7 @@ const Work = () => {
             {/*Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+                Janssen Enterprise
               </span>
               <div className="pt-8 text-center">
                 <a
@@ -73,6 +74,38 @@ const Work = () => {
               </div>
             </div>
           </div>
+
+          <div
+            style={{ backgroundImage: `url(${DataPulse})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/*Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                DataPule
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://sunny-muffin-57a601.netlify.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/towa0/Data_Pulse_app"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+
         </div>
       </div>
     </div>
