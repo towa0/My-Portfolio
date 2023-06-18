b2783616d4637583e987bf0951e221ccb0c831f4
towa0 <126798279+towa0@users.noreply.github.com>
2023-06-18 15:07:16 +0400

    Update Skills.jsx
    removed node.js (not confident in this skill yet)

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -42,10 +42,7 @@ const Skills = () => {
             <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
             <p className="my-4">Tailwind</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={NodeJs} alt="HTML icon" />
-            <p className="my-4">NodeJS</p>
-          </div>
+          
         </div>
       </div>
     </div>
